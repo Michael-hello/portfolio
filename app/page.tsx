4f2064e8a00407032f260a7eb22f0cec194fc9ba
Mike Jones <mjones@robertson-geo.com>
2026-04-06 02:47:46 +0100

    added google analytics

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,15 @@ import SkillsSection from './components/SkillsSection';
 import ProjectsSection from './components/ProjectsSection';
 import ContactSection from './components/ContactSection';
 
+import ReactGA from 'react-ga4';
+const TRACKING_ID = "G-21STSKJ9L4"; // OUR_TRACKING_ID
+
 export default function SimplePortfolio() {
+
+	ReactGA.initialize(TRACKING_ID);
+	ReactGA.send({ hitType: "pageview", page: "/home", title: "home page viewed" });
+
+
 	return (
 		<main className="min-h-screen bg-gray-900 pt-12">
 			<HeroSection />
